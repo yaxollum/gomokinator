da74d2df8823ceb74079673a051c50df8892d251
Peter Ye <yaxollum@gmail.com>
2023-02-02 17:19:32 -0500

    use canvas instead of pixi

--- src/playPage.ts
+++ src/playPage.ts
@@ -1,25 +1,31 @@
-import * as PIXI from "pixi.js";
 import { settings } from "./settings";
-function pixi() {
-  //Create a Pixi Application
-  const app = new PIXI.Application({
-    width: 256,
-    height: 256,
-    autoDensity: true,
-  });
-
-  //Add the canvas that Pixi automatically created for you to the HTML document
-  document.body.appendChild(app.view as HTMLCanvasElement);
-}
 
 export function playMain() {
-  const boardContainer = document.getElementById("main-board")!;
-  const app = new PIXI.Application({
-    resizeTo: boardContainer,
-    backgroundAlpha: 0,
-  });
-  boardContainer.appendChild(app.view as HTMLCanvasElement);
+  const c = document.getElementById("main-board") as HTMLCanvasElement;
   document.body.style.backgroundColor = settings.boardColour.get();
+
+  const dpi = 5;
+  const w = c.width;
+  c.width *= dpi;
+  c.height = c.width;
+  let ctx = c.getContext("2d")!;
+  ctx.scale(dpi, dpi);
+
+  const boardMargin = 0.5;
+  const boardSize = 15;
+  const boxSize = w / (boardMargin * 2 + boardSize - 1);
+  ctx.lineWidth = boxSize / 20;
+  let conv = (x: number) => (x - 1 + boardMargin) * boxSize;
+  let drawLine = (x1: number, y1: number, x2: number, y2: number) => {
+    ctx.moveTo(conv(x1), conv(y1));
+    ctx.lineTo(conv(x2), conv(y2));
+  };
+  ctx.beginPath();
+  for (let i = 1; i <= boardSize; ++i) {
+    drawLine(i, 1, i, boardSize);
+    drawLine(1, i, boardSize, i);
+  }
+  ctx.stroke();
 
   const player1Info = document.getElementById("player1-info")!;
   const player2Info = document.getElementById("player2-info")!;
